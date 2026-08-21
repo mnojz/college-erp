55a84363d1f4e0ef51761dd7ae4169ab59e36541
mnojz <manoj-joshi.mj@proton.me>
2026-08-22 00:05:31 +0545

    small cleanup.

---
+++ app/pritam/page.tsx
@@ -0,0 +1,13 @@
+export default function PritamPage() {
+  return (
+    <main className="flex flex-col justify-center items-center mt-25">
+      <h1 className="text-3xl">
+        I am proud of you pritam you got so far, <br /> 
+        i am really proud of you. <br />
+        this is a big achievement you cloned the repo <br />
+        you installed the node modules you run the project in dev environment <br /> 
+        and now you are seeing this page, this is really something. good luck
+      </h1>
+    </main>
+  );
+}
